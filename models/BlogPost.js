5ed7b57f6cc886db3f165ca051fcda5a875c194e
Matheus Alves <malves224@gmail.com>
2022-02-05 18:32:43 -0300

    configura models blogPost e postsCategorie para que o relacionamento N:N Funcione

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
 
   BlogPost.associate = (models) => {
     BlogPost.belongsTo(models.User, {
-      foreignKey: 'userId',
+      foreignKey: 'userId', as: 'user',
     });
   };
 
--- models/PostsCategorie.js
+++ models/PostsCategorie.js
@@ -1,20 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
   const PostsCategorie = sequelize.define('PostsCategorie',
-    {
-      postId: DataTypes.INTEGER, categoryId: DataTypes.INTEGER, 
-    },
+    { postId: DataTypes.INTEGER, categoryId: DataTypes.INTEGER },
     { timestamps: false });
 
   PostsCategorie.associate = (models) => {
     models.Categorie.belongsToMany(models.BlogPost, {
       through: PostsCategorie,
-      foreignKey: 'postId',
-      otherKey: 'categoryId',
+      foreignKey: 'categoryId',
+      as: 'posts',
+      otherKey: 'postId',
     });
     models.BlogPost.belongsToMany(models.Categorie, {
       through: PostsCategorie,
-      foreignKey: 'categoryId',
-      otherKey: 'postId',
+      as: 'categories',
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
     });
   };
 
